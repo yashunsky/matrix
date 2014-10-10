79ed0bc0514634909c9032f8732829a108e759c1
Vladimir Yashunsky <vladimir.yashunsky@gmail.com>
2014-10-10 23:52:14 +0400

    animation put into a separated thread

--- matrix.py
+++ matrix.py
@@ -11,14 +11,19 @@ from terminalsize import get_terminal_size
 
 from random import randint
 
+EXIT_TEXT = 'red pill'
+
 class MatrixOutput(object):
     """docstring for MatrixOutput"""
     def __init__(self):
         self.stdout = sys.stdout
         self.width, self.height = get_terminal_size()
         self.buffers = ['' for i in xrange(self.width)]
-
+        self.in_matrix = True
+        self.animation_thread = Thread(target=self.animation)
         self.clrscr()
+
+        self.animation_thread.start()
 
     def clrscr(self):
         self.screen = [[' ' for i in xrange(self.width)] for j in xrange(self.height)]
@@ -28,7 +33,13 @@ class MatrixOutput(object):
         self.stdout.write(text)
         self.stdout.flush()
 
-    def animate(self, step=1):
+    def animation(self):
+        while self.in_matrix:
+            self.animation_step()
+            sleep(0.2)
+        sys.stdout = self.stdout
+
+    def animation_step(self, step=1):
         for column in xrange(self.width):
             column_string = ''.join([self.screen[row][column] for row in xrange(self.height)])
             column_string = ' '*step + self.buffers[column] + column_string
@@ -43,6 +54,9 @@ class MatrixOutput(object):
         text = text.rstrip()
         if len(text) == 0: return
 
+        if text == EXIT_TEXT:
+            self.in_matrix = False
+
         column = randint(0, self.width)
         self.buffers[column] = text
         
@@ -51,8 +65,9 @@ class MatrixOutput(object):
 if __name__ == '__main__':
     sys.stdout = MatrixOutput()
     print 'Wake up, Neo.'
+    sleep(2)
     print 'The Matrix has you...'
+    sleep(2)
     print 'Follow the white rabbit'
-    for i in xrange(50):
-        sys.stdout.animate()
-        sleep(0.1)
+    sleep(5)
+    print 'red pill'
