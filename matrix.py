4cd22bec93a094e60276011607adfaa88647e469
Vladimir Yashunsky <vladimir.yashunsky@gmail.com>
2014-10-10 23:06:00 +0400

    screen table added

--- matrix.py
+++ matrix.py
@@ -3,12 +3,32 @@
 
 import sys
 
+from threading import Thread
+
+from terminalsize import get_terminal_size
+
 class MatrixOutput(object):
     """docstring for MatrixOutput"""
     def __init__(self):
         self.stdout = sys.stdout
+        self.width, self.height = get_terminal_size()
+        
+        self.clrscr()
+
+    def clrscr(self):
+        self.screen = [[' ' for i in xrange(self.width)] for j in xrange(self.height)]
+
+    def print_screen(self):      
+        self.stdout.write(''.join([''.join(line) for line in self.screen]))
+
     def write(self, text):
-        self.stdout.write(text)
+        text = text.rstrip()
+        if len(text) == 0: return
+
+        for i in xrange(self.height):
+            self.screen[i][i] = '*'
+        self.print_screen()
+        #self.stdout.write(text)
 
 if __name__ == '__main__':
     sys.stdout = MatrixOutput()
