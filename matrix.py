32e2c6c853d4928bad0f2e13d4c14f302ac86a14
Vladimir Yashunsky <vladimir.yashunsky@gmail.com>
2014-10-10 23:22:08 +0400

    text to column translation

--- matrix.py
+++ matrix.py
@@ -5,31 +5,38 @@ import sys
 
 from threading import Thread
 
+from time import sleep
+
 from terminalsize import get_terminal_size
+
+from random import randint
 
 class MatrixOutput(object):
     """docstring for MatrixOutput"""
     def __init__(self):
         self.stdout = sys.stdout
         self.width, self.height = get_terminal_size()
-        
+
         self.clrscr()
 
     def clrscr(self):
         self.screen = [[' ' for i in xrange(self.width)] for j in xrange(self.height)]
 
-    def print_screen(self):      
-        self.stdout.write(''.join([''.join(line) for line in self.screen]))
+    def print_screen(self):
+        text = ''.join([''.join(line) for line in self.screen])      
+        self.stdout.write(text)
 
     def write(self, text):
         text = text.rstrip()
         if len(text) == 0: return
 
-        for i in xrange(self.height):
-            self.screen[i][i] = '*'
+        column = randint(0, self.width)
+        for row, letter in enumerate(text):
+            self.screen[row + 1][column] = letter
+        
         self.print_screen()
-        #self.stdout.write(text)
 
 if __name__ == '__main__':
     sys.stdout = MatrixOutput()
-    print 'Wake up, Neo. The matrix has you'
+    print 'Wake up, Neo.'
+    print 'The Matrix has you...'
